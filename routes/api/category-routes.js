75a5c8963acc7f6717f73c0bfa090e9dff8f0faa
Cloud <m.melanson000@gmail.com>
2022-08-30 17:46:21 -0400

    finished category get all and get by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this ID' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch {
+      res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
