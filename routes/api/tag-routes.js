ae2fa4134d713225bdaa2f0818c84c8f5dfb8656
Cloud <m.melanson000@gmail.com>
2022-08-31 11:59:58 -0400

    completed tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,12 +36,44 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+
+    res.status(200).json(newTagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const tagData = await Tag.findByPk(req.params.id);
+    const updateTagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: `No tag found with ID: ${req.params.id}` });
+      return;
+    }
+  
+    if (!updateTagData[0]) {
+      res.status(400).json({ message: `No tag data updated for ID: ${req.params.id}` });
+      return;
+    };
+
+    res.status(200).json(updateTagData);
+  } catch {
+    res.status(500).json(err);
+  };
+
 });
 
 router.delete('/:id', async (req, res) => {
