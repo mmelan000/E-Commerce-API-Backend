6007b698b99afab4c2cba00148e0327535236e94
Cloud <m.melanson000@gmail.com>
2022-08-31 11:16:44 -0400

    completed product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,7 +108,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const deleteProductData = await Product.destroy({
